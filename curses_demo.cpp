0db019c11bf5c5be78452771e83711e05b57368f
Maciej Gajewski <maciej.gajewski0@gmail.com>
2022-11-05 18:02:47 +0100

    cloured animation

--- curses_demo.cpp
+++ curses_demo.cpp
@@ -3,10 +3,40 @@
 int main(int, char **) {
 
   initscr(); // initialize screen
+  noecho();
+  raw();
+  keypad(stdscr, TRUE);
+  curs_set(0);
+  start_color();
 
-  // code goes here
-  printw("Hello World !!!");
-  refresh();
-  getch();
+  init_pair(1, COLOR_RED, COLOR_BLACK);
+  init_pair(2, COLOR_GREEN, COLOR_BLACK);
+  attron(COLOR_PAIR(2));
+
+  int row = 5;
+  int col = 10;
+
+  while (true) {
+    mvprintw(row, col, "*");
+    int i = getch();
+    mvprintw(row, col, " ");
+
+    if (i == KEY_LEFT)
+      col--;
+    else if (i == KEY_RIGHT)
+      col++;
+    else if (i == KEY_UP)
+      row--;
+    else if (i == KEY_DOWN)
+      row++;
+    else if (i == 'g')
+      attron(COLOR_PAIR(2));
+    else if (i == 'r')
+      attron(COLOR_PAIR(1));
+
+    if (i == KEY_END)
+      break;
+  }
+
   endwin(); // disable curses, clear screen
 }
